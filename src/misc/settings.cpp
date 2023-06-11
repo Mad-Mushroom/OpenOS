4f0b4ef2b1bb3d30d5d398d431f89e34c553abd0
Mad-Mushroom <a.no.budy1@gmail.com>
2023-06-11 17:43:31 +0200

    started with settings

---
+++ src/misc/settings.cpp
@@ -0,0 +1,15 @@
+/* OpenOS Settings
+ *
+ * Made by MadMushroom
+ * 
+ * Copyright (c) 2023 MadMushroom
+ */
+
+#pragma once
+
+#include "../Kernel.h"
+
+class Settings {
+    public:
+        uint_8 theme;
+};
